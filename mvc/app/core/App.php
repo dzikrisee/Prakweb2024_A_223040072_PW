8c430de3a1c75c3fd93bde39b70f6ba50f04b4f2
dzikrisee <marlianawatiazzahra@gmail.com>
2024-10-09 01:07:16 +0700

    video #3 Routing

--- mvc/app/core/App.php
+++ mvc/app/core/App.php
@@ -1,11 +1,18 @@
-<?php 
+<?php
 
-class App {
+class App
+{
     public function __construct()
     {
-        var_dump($_GET);
+        $url = $this->parseURL();
+        var_dump($url);
     }
 
+    public function parseURL()
+    {
+        if (isset($_GET['url'])) {
+            $url = $_GET['url'];
+            return $url;
+        }
+    }
 }
-
-?>
