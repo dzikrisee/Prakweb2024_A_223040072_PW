7a35864862379846fbc887c315cab3c65687bce8
dzikrisee <marlianawatiazzahra@gmail.com>
2024-10-24 23:08:14 +0700

    Video #8 Overriding

--- oop/overriding.php
+++ oop/overriding.php
@@ -5,18 +5,14 @@ class Produk
     public $judul,
         $penulis,
         $penerbit,
-        $harga,
-        $jmlHalaman,
-        $waktuMain; // Tanda koma dihapus, diganti titik koma
+        $harga;
 
-    public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0)
+    public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0)
     {
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
-        $this->jmlHalaman = $jmlHalaman;
-        $this->waktuMain = $waktuMain;
     }
 
     public function getLabel()
@@ -34,6 +30,15 @@ class Produk
 
 class Komik extends Produk
 {
+    public $jmlHalaman;
+
+    public function __construct( $judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0, $jmlHalaman = 0 )
+    {
+        parent::__construct( $judul, $penulis, $penerbit, $harga );
+
+        $this->jmlHalaman = $jmlHalaman;
+    }
+
     public function getInfoProduk()
     {
         $str = "Komik : " . parent::getInfoProduk() . " - {$this->jmlHalaman} Halaman.";
@@ -44,9 +49,19 @@ class Komik extends Produk
 
 class Game extends Produk
 {
+
+    public $waktuMain;
+
+    public function __construct( $judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0, $waktuMain = 0 )
+    {
+       parent::__construct($judul, $penulis, $penerbit, $harga);
+
+       $this->waktuMain = $waktuMain;
+    }
+
     public function getInfoProduk()
     {
-        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->waktuMain} Jam.";
+        $str = "Game : " . parent::getInfoProduk() . " ~ {$this->waktuMain} Jam.";
 
         return $str;
     }
@@ -61,8 +76,8 @@ class CetakInfoProduk
     }
 }
 
-$produk1 = new Komik("One Piece", "Eiichiro Oda", "Shueisha", 50000, 100, 0);
-$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50);
+$produk1 = new Komik("One Piece", "Eiichiro Oda", "Shueisha", 50000, 100);
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
 
 echo $produk1->getInfoProduk();
 echo "<br>";
